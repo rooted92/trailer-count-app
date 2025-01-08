55be06a1c2f1ef72244bfe629be541a3136a2ecb
Pedro Castaneda <petecast76@gmail.com>
2025-01-07 18:08:15 -0800

    feat: add navbar, footer to partials, fix ES modules errors

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const path = require('path');
-const port = 3000;
+import { fileURLToPath } from 'url';
+import path from 'path';
+import trailers from './public/services/data.js';
 
-import { trailers } from './public/services/data.js';
-console.log(trailers);
+const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 let newTrailerData = [];
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.listen(port, () => {
+    console.log('Listening on PORT: 3000');
+});
